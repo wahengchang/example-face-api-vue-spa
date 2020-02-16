fc3ff7510d9c8eb2f14bcc641f5e2372c0eccdfb
wahengchang <wahengchang@gmail.com>
2020-02-16 17:58:30 +0800

    import page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Facedetection from '../views/Facedetection.vue'
+import Landmark from '../views/Landmark.vue'
 
 Vue.use(VueRouter)
 
@@ -13,12 +15,12 @@ const routes = [
   {
     path: '/facedetection',
     name: 'Facedetection',
-    component: () => import(/* webpackChunkName: "Facedetection" */ '../views/Facedetection.vue')
+    component: Facedetection
   },
   {
     path: '/landmark',
     name: 'Landmark',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Landmark.vue')
+    component: Landmark
   }
 ]
 
